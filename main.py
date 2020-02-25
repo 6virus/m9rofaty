f39a93b4549fad1021ed31097130b1e274d54268
Abdullanz <amnjrx@gmail.com>
2020-02-25 22:51:01 +0300

    fixed an issue with input and added little details

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Description: M9rofaty is an IOS application that helps the user to financially plan
 # their budget on a monthly basis.
 # Version: 0.1
-# Bugs: N/A
+# Bugs: allow CalculateCategories to take input & do its thing
 # ///////////////////////////////////////////////////////////////////////////////////
 
 # Introduction to the application
@@ -13,15 +13,19 @@ first = input('What is your name?\n')
 # User friendly
 print('Hi '+first)
 income = input('Enter your monthly income?\n')
-
+#CalculateCategories(income)
 
 # Divide up the salary for the best case senario to visualize it for the user
 def CalculateCategories(arg):
-    if ArithmeticError
+    # if ArithmeticError
+    print("qoq!")
 CalculateCategories()
 
 # I/O function
-
+# def CalculateCategories(arg):
+#     # if ArithmeticError
+#     print("qoq!")
+# CalculateCategories()
 
 
 
